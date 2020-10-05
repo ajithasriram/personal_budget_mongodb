773eb74dbedecf430771aaef0aebdbe71a9a6485
ajithasriram <ajithasriram@gmail.com>
2020-10-04 20:16:23 -0400

    Install cors and stop render static pages from server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// Budget API
 const express = require('express');
-const fs = require('fs')
+const fs = require('fs');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+app.use(cors());
 
 let budget = {}
 
@@ -12,14 +14,10 @@ fs.readFile('./dataset.json', 'utf-8', (err, data) => {
     budget = JSON.parse(data)
 })
 
-app.get('/hello', (req, res) => {
-    res.send('Konnichiwa!');
-});
-
 app.get('/budget', (req, res) => {
     res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`API served at http://localhost:${port}`);
 });
